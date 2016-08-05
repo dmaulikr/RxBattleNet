aaed6d8c26736540c4a53fcc7b5cba790b06750f
Ivan Fabijanovic <feb@bellabeat.com>
2016-08-05 19:54:54 +0200

    [Project] Added a new iOS Framework project (Swift, tests enabled)

---
+++ RxBattleNet/RxBattleNet.h
@@ -0,0 +1,19 @@
+//
+//  RxBattleNet.h
+//  RxBattleNet
+//
+//  Created by Ivan Fabijanović on 05/08/16.
+//  Copyright © 2016 Ivan Fabijanovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxBattleNet.
+FOUNDATION_EXPORT double RxBattleNetVersionNumber;
+
+//! Project version string for RxBattleNet.
+FOUNDATION_EXPORT const unsigned char RxBattleNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxBattleNet/PublicHeader.h>
+
+
